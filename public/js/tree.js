0162741010745c8a1e23e3f792c8b645578a7644
aurora <lgzamz@gmail.com>
2018-09-11 22:05:59 -0500

    segundoCommit

--- public/js/tree.js
+++ public/js/tree.js
@@ -3,23 +3,26 @@ $(document).ready(function () {
         "plugins": ["checkbox"]
     });
 });
+
+// 
 var test = function () {
     console.log('test');
-    console.log($("#div_tree").jstree("get_selected", true));
-    //
-    var respuesta = null;
+    
+    // 
+    var jsonArray = [];
+    var arreglo = $("#div_tree").jstree("get_selected", true); // obtener elementos seleccionados
+    for (let i in arreglo) {
+        console.log("Id: "+arreglo[i].id+", "+"Texto: "+arreglo[i].text);
+        jsonArray.push({'id':arreglo[i].id, 'texto':arreglo[i].text});
+    }
+
+    console.log(jsonArray);
+    // 
     $.get("http://127.0.0.1:8000/riesgos", function (data) {
         // $(".result").html(data);
-        respuesta = data;
         console.log('data', data);
     },function(error){
         console.log(error);
     });
-    console.log('despues de .get', respuesta);
 
 }
-// $(function() {
-//     $('#container').jstree({
-//         "plugins" : ["checkbox"]
-//     });
-// });
